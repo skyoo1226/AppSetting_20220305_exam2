a729f118efbcd8c62c6572aa960000616bc916d7
skyoo1226 <you1226@naver.com>
2022-03-05 21:47:46 +0900

    앱 초기 로딩 화면 추가

---
+++ app/src/main/java/com/skyoo/appsetting_20220305_exam2/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.skyoo.appsetting_20220305_exam2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
